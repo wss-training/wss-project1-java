894f9b8fc7347ea495b81723e116564c8970020c
katerinaozerova <53837535+katerinaozerova@users.noreply.github.com>
2021-02-25 10:31:50 -0500

    Add files via upload

---
+++ src/main/java/test/Hello.java
@@ -0,0 +1,6 @@
+package test;
+
+
+interface Hello {
+    public void eat();
+}
---
+++ src/main/java/test/Hello3.java
@@ -0,0 +1,10 @@
+package test;
+
+import io.netty.handler.ssl.OpenSslEngine;
+
+public class Hello3 extends Hello2 {
+
+    public void print(){
+        System.out.println("bla");
+    }
+}
